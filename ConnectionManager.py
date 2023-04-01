96771c00a028a734526a381b660a98170edda875
Szymon Gołębiowski <szymon13gol@gmail.com>
2023-04-01 14:23:50 +0200

    messages

--- ConnectionManager.py
+++ ConnectionManager.py
@@ -9,16 +9,16 @@ class ConnectionManager:
         
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        connection_message = await websocket.receive_text()
-        # json_object = json.loads(json.loads(connection_message))
-        json_object = json.loads(connection_message)
-        print(json_object)
+        # connection_message = await websocket.receive_text()
+
+        json_object = await self.safely_receive_message(websocket)
+        print("NEW USER: ", json_object)
+        
         name = json_object["name"]
         #TODO: sprawdzenie nicku
         #TODO: blokowanie zasobu
         self.sockets[name] = websocket
-
-        await self.receive_messages(websocket)
+        await self.receive_messages_from(name)
         
     async def broadcast(self, message: dict):
         for player in self.sockets.keys():
@@ -27,14 +27,17 @@ class ConnectionManager:
     def get_sockets(self):
         return self.sockets
 
-    async def receive_messages(self, websocket: WebSocket):
+    async def receive_messages_from(self, name: str):
+        socket = self.sockets[name]
         while True:
-            data = await websocket.receive_text()
-            print(data)
+            data = await self.safely_receive_message(socket)
             await self.queue.put(data)
 
-    def safely_parse_message(message):
-        result = json.loads(message)
-        if type(result) != dict:
-            result = json.loads(message)
-        return result
+    async def safely_receive_message(self, websocket : WebSocket):
+        message = await websocket.receive_json()
+        return message
+        # result = json.loads(message)
+        # if type(result) != dict:
+        #     result = json.loads(message)
+        # return result
+    
--- restapi.py
+++ restapi.py
@@ -10,13 +10,11 @@ import json
 app = FastAPI()
 
 room_manager = RoomManager()
-
-
+room = Room(1, "")
+room_manager.add_room(room)
 
 @app.get("/")
 async def root():
-    room = Room(1, "")
-    room_manager.add_room(room)
     await room.engine()
     return "Up and running"
 
