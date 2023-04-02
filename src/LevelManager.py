167c681dc470d7ec6b6d60b7dc19fd7bdd3a4547
Arch4ngel21 <adammichal657@gmail.com>
2023-04-02 15:51:21 +0200

    Aggregated receiving responses in fetch_all.

--- src/LevelManager.py
+++ src/LevelManager.py
@@ -1,6 +1,9 @@
+import asyncio
+
 from httpx import AsyncClient
 import json
 from random import choice
+
 
 class LevelManager:
     def __init__(self):
@@ -8,34 +11,38 @@ class LevelManager:
         self.URL = "http://localhost:8001/"
         self.categories = {}
         self.images = []
-    
+
     def generate_level(self, stages: int, stage_size: int):
         return choice(self.images)
-    
+
     async def fetch_all(self, stages: int, stage_size: int):
         await self.fetch_categories()
-        
+
+        tasks = []
         for key in self.categories.keys():
-            response = await self.fetch_images(stages, stage_size, key)
+            tasks.append(self.fetch_images(stages, stage_size, key))
+
+        responses = await asyncio.gather(*tasks)
+        json_responses = [
+            json.loads(response.json()) for response in responses
+        ]
+        for response in json_responses:
             self.images.append(response)
-        
+
     async def fetch_categories(self):
         url = self.URL + "category"
         response = await AsyncClient().get(url)
         self.categories = json.loads(response.json())
-        
+
     async def fetch_images(self, stages: int, stage_size: int, key: str):
-        request_body = {
-            'stages': stages,
-            'stage_batch_size': stage_size
-        }
+        request_body = {"stages": stages, "stage_batch_size": stage_size}
         url = self.URL + "category/"
         client = AsyncClient()
-        body = json.dumps(request_body).encode('utf-8')
+        body = json.dumps(request_body).encode("utf-8")
         if key == "":
             url += "random"
-            response = await client.post(url, content=body)
-            return json.loads(response.json())
+            return client.post(url, content=body)
+            # return json.loads(response.json())
         else:
-            response = await client.post(url + key, content=body)
-            return json.loads(response.json())
+            return client.post(url + key, content=body)
+            # return json.loads(response.json())
